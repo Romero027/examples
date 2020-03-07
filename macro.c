931aad908aef07a5eba9a11f6381632db8558817
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-03-07 02:01:48 -0500

    update

---
+++ macro.c
@@ -0,0 +1,60 @@
+// To test this file, run gcc -Wall -save-temps macro.c -o macro and check macro.i
+
+#include<stdio.h>
+
+#define MAX_SIZE 10
+#define SWAP(a,b)({a ^= b; b ^= a; a ^= b;})
+
+int main(void)
+{
+   int size = 0;
+   size = size + MAX_SIZE;
+
+   printf("\n The value of size is [%d]\n",size);
+
+   int x = 10;
+   int y = 20;
+   SWAP(x, y);
+   printf("After swap, x is %d and y is %d\n", x, y);
+
+   #define TEST3 1
+
+   // Code snippets are guarded with a condition checking if a certain macro is defined or not
+   #ifdef TEST3
+   printf("MACRO TEST3 is defined\n");
+   #else
+   printf("MACRO TEST3 is NOT defined\n");
+   #endif
+
+   // A simple trick to commet multiple lines
+   #if 0
+   printf("Test\n");
+   #endif
+   return 0;
+}
+
+/*
+The above code will be replaced by the compiler(on my mac) as
+int main(void)
+int main(void)
+{
+   int size = 0;
+   size = size + 10;
+
+   printf("\n The value of size is [%d]\n",size);
+
+   int x = 10;
+   int y = 20;
+   ({x ^= y; y ^= x; x ^= y;});
+   printf("After swap, x is %d and y is %d\n", x, y);
+
+
+
+
+
+   printf("MACRO TEST3 is defined\n");
+# 35 "macro.c"
+   return 0;
+}
+}
+*/
