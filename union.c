68944428c1e3c87eda784b168059094ad50f3aca
Xiangfeng Zhu <xzhu0027@gmail.com>
2023-08-31 21:59:45 +0000

    update

--- union.c
+++ union.c
@@ -11,14 +11,12 @@ int main( ) {
 
     union Data data;        
 
-    // The size of the union will be large the largest element
     printf( "Memory size occupied by data : %lu\n", sizeof(data));
 
     data.i = 10;
     data.f = 220.5;
     strcpy( data.str, "C Programming");
 
-    // i and f members of union will be corrupted because only one member can contain a value at any given time
     printf( "data.i : %d\n", data.i);
     printf( "data.f : %f\n", data.f);
     printf( "data.str : %s\n", data.str);
